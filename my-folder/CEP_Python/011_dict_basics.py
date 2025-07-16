ac9545f239aee96817e60485b83c1db829b6403c
Praveenkumar Murugesan <praveenmurkesh@yahoo.in>
2025-07-16 22:08:22 +0530

    Adding Dictionary basics to git
    Signed-off-by: Praveenkumar Murugesan <praveenmurkesh@yahoo.in>

---
+++ my-folder/CEP_Python/011_dict_basics.py
@@ -0,0 +1,17 @@
+## Creating dictionary
+
+Student = {
+    "name" : "Praveen",
+    "age" : 22,
+    "Course" : "Python"
+}
+
+# print(Student["name"])
+# print(Student["age"])
+
+####Adding and changing value
+
+Student["email"] = "pravie@email.com"
+Student["age"] = 23
+
+print(Student)
---
+++ my-folder/CEP_Python/011_dict_loop.py
@@ -0,0 +1,8 @@
+student = {
+    "name": "Arun",
+    "age": 23,
+    "email": "arun@email.com"
+}
+
+for key, value in student.items():
+    print(f"{key}:{value}")
