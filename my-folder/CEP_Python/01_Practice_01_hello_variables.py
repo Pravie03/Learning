43fb808ee2290ebb2d6013ec617f924487ec263d
Pravie03 <praveenmurkesh@yahoo.in>
2025-07-18 08:53:08 +0530

    adding my learnings to git
    adding my learnings to git

---
+++ my-folder/CEP_Python/01_Practice_01_hello_variables.py
@@ -0,0 +1,6 @@
+print("Welcome to Python")
+name=input("what is your name? :")
+age=input("your age:")
+city=input("where are you from:")
+
+print("Hello!!",name,"from",city+"!","at the age of","age")
